3548bf80615aa47352b18a1ddd50ce89023ac2af
shubhamg <>
2024-09-25 20:15:07 +0530

    First git example practise

---
+++ FirstReact/app.js
@@ -0,0 +1,36 @@
+
+/*
+const heading= React.createElement("h1",{
+    id:'heading'
+},"Hi From React");
+*/
+
+
+const parent=React.createElement("div",{  id:"rootParent"},[
+        React.createElement("div",{id:"child1"},[
+            React.createElement("h1",
+                {id:"heading1"},
+                "First Heading"
+            ),
+            React.createElement("h1",
+                {id:"heading2"},
+                "Second Heading")
+        ]),
+        React.createElement("div",{id:"child2"},[
+            React.createElement("h1",
+                {id:"heading1"},
+                "Third Heading"
+            ),
+            React.createElement("h1",
+                {id:"heading2"},
+                "Fourth Heading")
+        ]),
+
+    ]        
+    )
+    
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
